24a62ae1a30abb2cf8c57d34f186f04c77a68f1a
erripollo <pollone.enrico@gmail.com>
2022-07-05 10:58:42 +0200

    create authReducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { environment } from './../environments/environment';
     BrowserModule,
     AppRoutingModule,
     AuthModule,
-    StoreModule.forRoot([]),
+    StoreModule.forRoot({}),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,11 @@
+import { StoreModule } from '@ngrx/store';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { RegisterComponent } from './components/register/register.component';
+import { reducers } from './store/reducers';
 
 const routes = [
     {
@@ -15,7 +17,8 @@ const routes = [
     imports: [
         CommonModule,
         RouterModule.forChild(routes),
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        StoreModule.forFeature('auth', reducers)
     ],
     declarations: [RegisterComponent]
 })
